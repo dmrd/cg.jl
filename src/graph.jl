e33b21a116ea36773793e3fcec1d312247bcb947
David Dohan <ddohan@cs.princeton.edu>
2015-11-27 21:03:32 -0500

    Autodiff draft, graph functions, start interpret

--- src/graph.jl
+++ src/graph.jl
@@ -12,13 +12,15 @@ immutable Graph
 end
 
 # OpType - specifies operator (e.g. mul, rand, zeros)
-#type Apply{T <: OpType} <: Node
-type Apply <: Node
+# The lack of mutually recursive types is annoying
+#type Apply <: Node
+type Apply{T <: Node} <: Node
     op::OpType
-    inputs::Vector{Node}
-    output::Node
+    inputs::Vector{T}
+    output::T
     name::Nullable{AbstractString}
 end
+
 
 #type Variable{V <: VarType} <: Node
 type Variable <: Node
@@ -26,6 +28,14 @@ type Variable <: Node
     clients::Vector{Apply}
     data::VarType
     name::Nullable{AbstractString}
+end
+
+inputs(n::Apply{Variable}) = n.inputs #::Vector{Variable} # Type weirdness
+output(n::Apply{Variable}) = n.output::Variable
+
+function name{T <: Node}(n::T, str::AbstractString)
+    n.name = Nullable(str)
+    n
 end
 
 function Variable(data::VarType, name::AbstractString="")
@@ -37,7 +47,7 @@ end
 function apply(op::OpType, inputs::Vector{Variable}, name::AbstractString="")
     newName = length(name) == 0 ? Nullable("$(gensym())") : Nullable(name)
     var = Variable(TensorVar())
-    apply = Apply(op, inputs, var, newName)
+    apply = Apply{Variable}(op, inputs, var, newName)
     var.owner = apply
     for i in inputs
         push!(i.clients, apply)
@@ -143,6 +153,8 @@ type Neg <: UnOp end
 
 type Transpose <: UnOp end
 
+type Assign <: OpType end
+
 
 -(a::Variable) = apply(Neg(), [a])
 Neg(a::Variable) = apply(Neg(), [a])
@@ -157,6 +169,8 @@ t(a::Variable) = apply(Transpose(), [a])
 .-(a::Variable, b::Variable) = apply(Sub(), [a, b])
 .*(a::Variable, b::Variable) = apply(Mul(), [a, b])
 ./(a::Variable, b::Variable) = apply(Div(), [a, b])
+
+.=(a::Variable, b::Variable) = apply(Assign(), [a, b])
 
 ### Gradients
 # Return vector of variables, where ith is result of gradient wrt input i
@@ -186,7 +200,7 @@ function grad(op::MatSub, inputs::Vector{Variable}, grad_out::Variable)
 end
 
 function grad(op::MatMul, inputs::Vector{Variable}, grad_out::Variable)
-    [grad_out * t(inputs[2]), grad_out * t(inputs[1])]
+    [grad_out * t(inputs[2]), t(inputs[1]) * grad_out]
 end
 
 function grad(op::Neg, inputs::Vector{Variable}, grad_out::Variable)
@@ -197,18 +211,81 @@ function grad(op::Transpose, inputs::Vector{Variable}, grad_out::Variable)
     [grad_out]
 end
 
-# Autodiff
-function grad(out, wrt)
-    
+function grad(op::Assign, inputs::Vector{Variable}, grad_out::Variable)
+    [grad_out]
 end
+
+# Autodiff
+function grad(graph::Graph, out::Variable, wrt::Vector{Variable})
+    # Set of nodes on all paths between the set `wrt` and `out`
+    # Not
+    downstream = influenced_by(wrt, true)
+    upstream = influenced_by([out], false)
+    on_path = intersect(upstream, downstream)
+
+    toposorted = toposort(graph)
+    node_to_grad = Dict{Variable, Variable}(out => out)
+    for node = reverse(toposorted)
+        if !(node in on_path)
+            continue
+        end
+        if isa(node, Variable)
+            # Variables should have grad calculated by the time we process them
+            @assert haskey(node_to_grad, node)
+        else
+            # Should have already computed output's gradient
+            @assert haskey(node_to_grad, node.output)
+            gradients = grad(node.op, inputs(node), node_to_grad[node.output])
+            for (original, gradient) in zip(inputs(node), gradients)
+                if original in on_path
+                    node_to_grad[original] = gradient
+                    if !isnull(original.name)
+                        name(gradient, "G:$(get(original.name))")
+                    end
+                    #original .= gradient
+                end
+            end
+        end
+    end
+    
+    node_to_grad
+end
+
+## TODO: Build better graph abstraction, avoid repeated code
+
+# Return set of nodes that are influenced in the DAG from any in set `nodes`
+# i.e. that would be influenced by in a computation
+# child=true means go to children in dag, false means go to parents
+function influenced_by(nodes::Vector{Variable}, child::Bool)
+    queue = Vector{Node}(nodes)
+    influenced = Set{Node}(nodes)
+    next_method = child ? succ : pred
+    while !isempty(queue)
+        next = pop!(queue)
+        for node = next_method(next)
+            if node in influenced
+                continue
+            end
+            push!(queue, node)
+            push!(influenced, node)
+        end
+    end
+    influenced
+end
+
+######
+# Operations
+######
+
 
 ######
 # Interpret
 ######
 
-function interpret(outputs::Vector{Variable}, input_nodes::Vector{Input}, arguments::Vector{Array{Float}})
+
+function interpret(graph::Graph, outputs::Vector{Variable}, input_nodes::Vector{Input}, arguments::Vector{Array{Float}})
     @assert length(input_nodes) == length(arguments)
-    frontier = Vector{Apply}()
+    frontier = Vector{Apply{Variable}}()
     values = Dict{Variable, Array{Float}}()
     graph = Graph(input_nodes)
 
@@ -219,7 +296,18 @@ function interpret(outputs::Vector{Variable}, input_nodes::Vector{Input}, argume
             @assert false && "Every noninput node must have a parent"
         end
     end
+
+    order = toposorted(graph)
+    for node = order
+        if isa(node, Variable)
+            
+        elseif isa(node, Apply)
+
+        end
+    end
 end
+
+## TODO: Transform to straight Julia source code
 
 ######
 # Graph operations
@@ -229,6 +317,7 @@ function dfs(seen::Vector{Node})
     # WRITE THIS
 end
 
+# Returns nodes in topological order
 function toposort(graph::Graph)
     result = Vector{Node}()
     marks = Dict{Node, Symbol}() # :marked, :temp
@@ -281,46 +370,41 @@ function getGraph(nodes::Vector{Variable})
     Graph(seen)
 end
 
-function pred(node::Apply)
-    node.inputs
-end
+pred(node::Apply) = inputs(node)
+succ(node::Apply) = [node.output]
+
+# function pred(node::Apply{T})
+#     node.inputs
+# end
+# function succ(node::Apply{T})
+#     [node.output]
+# end
 
 function pred(node::Variable)
     isnull(node.owner) ? [] : [get(node.owner)]
 end
 
-function succ(node::Apply)
-    [node.output]
-end
 
 function succ(node::Variable)
     node.clients
 end
 
-function label(node::Apply)
-    string(typeof(node.op))
-end
-
-function label(node::Variable)
-    string(typeof(node.data))
-end
-
 # Print connected component of node
 function toDot(node::Node)
-    nodes = connected(node)
+    G = getGraph([node])
     nodeIds = Dict{Node, Int}()
     id = 0
-    for node in nodes
+    for node in G.nodes
         nodeIds[node] = id
         id += 1
     end
 
     labels = Vector{AbstractString}()
     edges = Vector{AbstractString}()
-    for node in nodes
+    for node in G.nodes
         thisId = nodeIds[node]
-        shape = typeof(node) == Apply ? "box" : "ellipse"
-        labelLine = string(thisId, " [shape=\"", shape,"\", label=\"", label(node), "\"];")
+        shape = isa(node, Apply) ? "box" : "ellipse"
+        labelLine = string(thisId, " [shape=\"", shape,"\", label=\"", toString(node), "\"];")
         push!(labels, labelLine)
         for next in succ(node)
             edge = "$(nodeIds[node]) -> $(nodeIds[next]);"
